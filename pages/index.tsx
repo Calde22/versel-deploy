f74b179137d2952903f08f76aeb79ba549c7add5
Gustavo Alberto Calderon <gcaldero@co.ibm.com>
2022-05-31 13:48:20 -0500

    cambio de Link a link en los componentes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/Link'
+import Link from 'next/link'
 import { MainLayout } from '../components/layouts/MainLayout'
 
 export default function Home() {
